fa5b7771246e3924972ec3e5ec4e14edf7efb6f4
Nico Singh <4417761+nicosingh@users.noreply.github.com>
2025-09-10 15:56:20 -0300

    Refactor Terraform workflows: remove old init workflow, add multi-environment apply and destroy workflows

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 module "infraestructura_azure" {
   source = "git::https://github.com/nicosingh/terraform-azurerm-duoc-iac-sep-25.git?ref=03c55adb6449b915a92db38491a1878b5e58ff65"
 
-  nombre_proyecto = "clase-8"
-  ubicacion       = "eastus2"
-  ambiente        = "dev"
-
-  nombre_grupo_recursos = "AreaInfraestructura"
-
+  nombre_proyecto             = "clase-8-${terraform.workspace}"
+  ubicacion                   = "eastus2"
+  ambiente                    = terraform.workspace
+  nombre_grupo_recursos       = "AreaInfraestructura"
   crear_cuenta_almacenamiento = true
   crear_maquina_virtual       = true
+  bloque_red                  = "10.0.0.0/16"
 }
